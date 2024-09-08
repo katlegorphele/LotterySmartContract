3d252832423e71f1fecfd06f7924aca36697b92a
katlegorphele <katlegorphele@gmail.com>
2024-09-08 12:14:52 +0200

    Connection to Chainlink complete

--- LotteryContract.sol
+++ LotteryContract.sol
@@ -1,31 +1,87 @@
-// SPDX-License-Identifier: MI
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.17;
 
-import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
 
-contract Lottery is VRFConsumerBase {
-    bytes32 internal keyHash; // 
-    uint256 internal fee;
-    uint256 public randomResult;
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts@1.2.0/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts@1.2.0/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
 
 
-    address public owner;
+
+contract Lottery is VRFConsumerBaseV2Plus{
+    
+    // Original Lottery Setup Params
     address payable[] public players;
     uint public lotteryId;
     mapping (uint => address payable) public lotteryHistory;
 
+    struct RequestStatus {
+        bool fulfilled; // wether the request has been fulfilled
+        bool exists; // wether a requestID exists
+        uint256[] randomWords;
+    }
 
+    mapping(uint256 => RequestStatus)
+        public s_requests; // requestId --> requestStatus
+    
+    // Chainlink specific variables
+    uint256 public s_subscriptionId; // From subscription manager
+    uint256[] public requestIds; // Past requests
+    uint256 public lastRequestId;
+    bytes32 public keyHash = 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae;
+    uint32 callbackGasLimit = 100000;
+    uint16 public requestConfirmations = 3; // number of blocks that must be mined before returning a number
+    uint32 public numWords = 2; // Number of random numbers we want back
 
-    constructor() 
-        VRFConsumerBase(
-            0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B,
-            0x779877A7B0D9E8603169DdbD7836e478b4624789
-        ) 
-    {
-        owner = msg.sender;
-        lotteryId = 1;
-        keyHash = 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae;
-        fee = 0.1 * 10 ** 18;
+    constructor(uint256 subscriptionId) VRFConsumerBaseV2Plus(0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B) {
+        s_subscriptionId = subscriptionId;
+    }
+
+    function requestRandomWords(
+        bool enableNativePayment
+    ) external onlyOwner returns (uint256 requestId) {
+        // Will revert if subscription is not set and funded.
+        requestId = s_vrfCoordinator.requestRandomWords(
+            VRFV2PlusClient.RandomWordsRequest({
+                keyHash: keyHash,
+                subId: s_subscriptionId,
+                requestConfirmations: requestConfirmations,
+                callbackGasLimit: callbackGasLimit,
+                numWords: numWords,
+                extraArgs: VRFV2PlusClient._argsToBytes(
+                    VRFV2PlusClient.ExtraArgsV1({
+                        nativePayment: enableNativePayment
+                    })
+                )
+            })
+        );
+        s_requests[requestId] = RequestStatus({
+            randomWords: new uint256[](0),
+            exists: true,
+            fulfilled: false
+        });
+        requestIds.push(requestId);
+        lastRequestId = requestId;
+        // emit RequestSent(requestId, numWords);
+        return requestId;
+    }
+
+    function fulfillRandomWords(
+        uint256 _requestId,
+        uint256[] calldata _randomWords
+    ) internal override {
+        require(s_requests[_requestId].exists, "request not found");
+        s_requests[_requestId].fulfilled = true;
+        s_requests[_requestId].randomWords = _randomWords;
+        // emit RequestFulfilled(_requestId, _randomWords);
+    }
+
+    function getRequestStatus(
+        uint256 _requestId
+    ) external view returns (bool fulfilled, uint256[] memory randomWords) {
+        require(s_requests[_requestId].exists, "request not found");
+        RequestStatus memory request = s_requests[_requestId];
+        return (request.fulfilled, request.randomWords);
     }
 
     function getWinnerByLottery(uint lottery) public view returns (address payable) {
@@ -41,29 +97,16 @@ contract Lottery is VRFConsumerBase {
     }
 
     function enter() public payable {
-        require(msg.value > .001 ether, "No fees");
+        require(msg.value > .001 ether);
         players.push(payable(msg.sender));
     }
 
-    // function getRandomNumber() public view returns (uint) {
-    //     return uint(keccak256(abi.encodePacked(owner, block.timestamp)));
-    // }
-
-    
-
-    function getRandomNumber() public onlyowner returns (bytes32 requestId) { //are we removing the modifier
-        require(LINK.balanceOf(address(this)) >= fee, "Not enough LINK");
-        return requestRandomness(keyHash, fee);
+    function getRandomNumber() public view returns (uint) {
+        return uint(keccak256(abi.encodePacked(msg.sender, block.timestamp)));
     }
-
-     function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
-        randomResult = randomness;
-        pickWinner();
-    }
-
 
     function pickWinner() public onlyowner {
-        uint index = randomResult % players.length;
+        uint index = getRandomNumber() % players.length;
         players[index].transfer(address(this).balance);
 
         lotteryHistory[lotteryId] = players[index];
@@ -75,7 +118,7 @@ contract Lottery is VRFConsumerBase {
     }
 
     modifier onlyowner() {
-      require(msg.sender == owner);
+      require(msg.sender == owner(), "Only the owner can run this function");
       _;
     }
 }
